c20aed5f2f5f45c250942fe6e0a93bb57ee17bc5
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-24 11:25:46 -0400

    complete excercise 1

---
+++ Functions/simple_functions.js
@@ -0,0 +1,11 @@
+function favoriteThings() {
+    console.log(`Name:Maria \nFavorite Show:The Evil Within \nFavorite Musician Hozier`);
+}
+
+function whyImHere() {
+    console.log(`I want to learn how to code`);
+}
+
+function favoritePlace() {
+    console.log(`There's a secret park in the uptown part of the city where the river is beautiful in the summer and the snow is a wonderland in the winter. Where? I'll never tell ;)`);
+}
