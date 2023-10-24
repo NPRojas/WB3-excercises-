8473d9b6ac1bc6f0efffd5743f9df7641db85e18
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-24 11:53:03 -0400

    complete excercise on 1-12 in the workbook

---
+++ Functions/more_functions.js
@@ -0,0 +1,25 @@
+function displayMailingLabel(name, address, city, state, zip) {
+    const addressLabel = `${name} \n${address} \n${city}, ${state} \n${zip}`
+    console.log(addressLabel);
+}
+
+function addNumbers(num1, num2) {
+    const add = num1 + num2
+    console.log(add);
+}
+
+function displayReceipt(totalDue, amountPaid) {
+    const changeDue = amountPaid - totalDue;
+    const reciept = `Total Due: $${totalDue} \nAmount Paid: $${amountPaid} \nChange Due: $${changeDue}`
+    console.log(reciept);
+}
+
+displayMailingLabel('Christina Castillo', '431 Murry Ave', 'New York', 'New York', '10531');
+displayMailingLabel('Sammy Lopez', '516 Garamond Place', 'Brooklyn', 'New York', '14852');
+
+addNumbers(5,2);
+addNumbers(4,4);
+
+displayReceipt(30,60);
+displayReceipt(30,30);
+displayReceipt(30,20);
