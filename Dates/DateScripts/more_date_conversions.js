d0bc4b25b3673418820267974e3e60c8fd5041f4
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-26 12:58:20 -0400

    complete excercise 2 on 2-32

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,7 @@
+const birthday = new Date('08/16/1980');
+const momBirthday = new Date('08 15 1952');
+const dadBirthday = new Date(1942, 10, 15);
+
+console.log(birthday.toLocaleString());
+console.log(momBirthday.toLocaleString());
+console.log(dadBirthday.toLocaleString());
